bb9eed558e4a2ed8e52aa0772585d120ef53ddce
Po1tos18 <92795035+Po1tos18@users.noreply.github.com>
2021-12-14 13:59:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let name = prompt('Введите свое имя')
+alert('Привет,'+(name)+'!')
+const year = 2021
+let birthday = prompt('Введите свой год рождения')
+alert((year)-(birthday))
+let a = prompt('Введите длину стороны квдрата')
+alert('P='+4*(a))
+const Пи = 3.14
+let r = prompt('Введите радиус окружности')
+alert('S='+(Пи)*(r)**2)
+let S = prompt('Введите расстояние между двумя городами в км')
+let t = prompt('Введите время за которое вы хотите добраться')
+alert('V='+(S)/(t)+'км/час')
